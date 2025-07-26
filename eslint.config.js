6b99bff6f11a5809a92c1214ec7168c091fcdada
Hossein Shakouri <mhshakouri@gmail.com>
2025-07-27 02:51:44 +0330

    chore(linter): added more strict and type aware rules for eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,8 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import reactX from 'eslint-plugin-react-x'
+import reactDom from 'eslint-plugin-react-dom'
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -11,11 +13,25 @@ export default tseslint.config([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      // tseslint.configs.recommended,
+      // type-aware lint rules
+      ...tseslint.configs.recommendedTypeChecked,
+      // stricter rules
+      ...tseslint.configs.strictTypeChecked,
+      // stylistic rules
+      ...tseslint.configs.stylisticTypeChecked,
+      // lint rules for React
+      reactX.configs['recommended-typescript'],
+      // rules for React DOM
+      reactDom.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
       ecmaVersion: 2020,
       globals: globals.browser,
     },
