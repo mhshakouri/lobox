769e63a4bf08d008debd2e98b2dbf0ca76cee419
Hossein Shakouri <mhshakouri@gmail.com>
2025-07-27 03:57:18 +0330

    chore(dropdown): removed unnecessary onInput function and state

--- src/components/MultiSelectDropdown/Dropdown.tsx
+++ src/components/MultiSelectDropdown/Dropdown.tsx
@@ -1,23 +1,22 @@
 import styles from './Dropdown.module.scss'
-import { useState, type ChangeEvent, type KeyboardEvent } from "react"
+import { useState, type KeyboardEvent } from "react"
 import MultiSelectDropdownOptions from './Options'
 import type { IMultiSelectDropdownOption } from './types'
 
 export default function MultiSelectDropdown() {
     const [options, setOptions] = useState<IMultiSelectDropdownOption[]>([])
-    const [inputValue, setInputValue] = useState<string>('')
 
-    const inputOnChange = (event: ChangeEvent) => { setInputValue((event.target as HTMLInputElement).value) }
-    
     const inputOnKeyDown = (event: KeyboardEvent) => { 
         if (event.key !== 'Enter')  return
-        setOptions([...options, { value: (event.target as HTMLInputElement).value, id: crypto.randomUUID()}])
+        const value = (event.target as HTMLInputElement).value
+        if (!value.length) return
+        setOptions([...options, { value, id: crypto.randomUUID()}])
     }
 
     return (
         <>
             <div className={styles.multiSelectDropdown}>
-                <input value={inputValue} onChange={inputOnChange} onKeyDown={inputOnKeyDown} />
+                <input onKeyDown={inputOnKeyDown} />
                 <MultiSelectDropdownOptions options={options} />
             </div>
         </>
