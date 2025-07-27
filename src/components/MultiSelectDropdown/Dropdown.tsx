f57632568e2cf58a0da3ac12100645abcc378f3c
Hossein Shakouri <mhshakouri@gmail.com>
2025-07-27 06:55:07 +0330

    feat(dropdown): added useClickOutside to dropdown and some other improvements

--- src/components/MultiSelectDropdown/Dropdown.tsx
+++ src/components/MultiSelectDropdown/Dropdown.tsx
@@ -1,41 +1,67 @@
-import styles from './Dropdown.module.scss'
-import { useState, type KeyboardEvent } from "react"
-import MultiSelectDropdownOption from './Option'
-import type { IMultiSelectDropdownOption } from './types'
-import MultiSelectDropdownInput from './Input'
+import styles from "./Dropdown.module.scss";
+import { useState } from "react";
+import MultiSelectDropdownOption from "./Option";
+import type { IMultiSelectDropdownOption } from "./types";
+import MultiSelectDropdownInput from "./Input";
+import useClickOutside from "../../hooks/useClickoutside";
 
 export default function MultiSelectDropdown() {
-    const [options, setOptions] = useState<IMultiSelectDropdownOption[]>([])
-    const [selections, setSelections] = useState<string[]>([])
+  const [options, setOptions] = useState<IMultiSelectDropdownOption[]>([]);
+  const [selections, setSelections] = useState<string[]>([]);
+  const [listOpen, setListOpen] = useState<boolean>(false);
 
-    const inputOnKeyDown = (event: KeyboardEvent) => { 
-        if (event.key !== 'Enter')  return
-        const value = (event.target as HTMLInputElement).value
-        if (!value.length) return
-        setOptions([...options, { value, id: crypto.randomUUID(), selected: false}])
+  const ref = useClickOutside<HTMLDivElement>(() => {
+    setListOpen(false);
+  });
+
+  const addNewItem = (value: string) => {
+    setOptions([
+      ...options,
+      {
+        value,
+        id: crypto.randomUUID(),
+        selected: false,
+      },
+    ]);
+    setListOpen(true);
+  };
+
+  const optionOnClick = (id: string) => {
+    setSelections(
+      selections.includes(id)
+        ? selections.filter((sel) => sel !== id)
+        : [...selections, id]
+    );
+  };
+
+  const toggleListOpen = (value?: boolean) => {
+    if (value !== undefined) {
+      setListOpen(value);
+      return;
     }
+    setListOpen(!listOpen);
+  };
 
-    const optionOnClick = (id: string) => {
-        setSelections(selections.includes(id) ? selections.filter(sel => sel !== id) : [...selections, id])
-    }
+  const optionsList = options.map(({ value, id }) => (
+    <MultiSelectDropdownOption
+      value={value}
+      key={id}
+      selected={selections.includes(id)}
+      onClick={() => {
+        optionOnClick(id);
+      }}
+    />
+  ));
 
-    const optionsList = options.map(({value, id}) => (
-        <MultiSelectDropdownOption 
-            value={value}
-            key={id}
-            selected={selections.includes(id)} 
-            onClick={() => { optionOnClick(id) }} 
-        />
-    ))
-
-    return (
-        <>
-            <div className={styles.dropdown}>
-                <MultiSelectDropdownInput onKeyDown={inputOnKeyDown} />
-                <ul>
-                    {optionsList}
-                </ul>
-            </div>
-        </>
-    )
+  return (
+    <div className={styles.dropdown} ref={ref}>
+      <MultiSelectDropdownInput
+        onSubmitValue={addNewItem}
+        canOpenList={Boolean(options.length)}
+        toggleList={toggleListOpen}
+        listOpen={listOpen}
+      />
+      {listOpen ? <ul className={styles.optionsList}>{optionsList}</ul> : <></>}
+    </div>
+  );
 }
