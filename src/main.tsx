77e89922cf0fcf20171875bd968a3e43ccf771bb
Hossein Shakouri <mhshakouri@gmail.com>
2025-07-27 05:52:37 +0330

    feat(cssreset): added scss reset

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.scss'
+import './reset.scss'
 import App from './App.tsx'
 
 const rootElement = document.getElementById('root')
