f4ab15d3e8c2e912ace53395739954fa85054e35
Hossein Shakouri <mhshakouri@gmail.com>
2025-07-27 05:59:33 +0330

    feat(input): implemented some functionality of input

--- src/components/MultiSelectDropdown/Input.tsx
+++ src/components/MultiSelectDropdown/Input.tsx
@@ -1,8 +1,64 @@
-import styles from './Input.module.scss'
+import {
+  useState,
+  type KeyboardEventHandler,
+  type ReactEventHandler,
+} from "react";
+import styles from "./Input.module.scss";
+import clx from "classnames";
 import type { IMultiSelectDropdownInputProps } from "./types";
+import capitalizeFirstLetter from "../../utils/capitalizeFirstLetter";
+import FaIcon from "../FaIcon/icon";
 
-export default function MultiSelectDropdownInput({onKeyDown}: IMultiSelectDropdownInputProps) {
-    return (
-        <input className={styles.input} type="text" onKeyDown={onKeyDown}/>
-    )
+export default function MultiSelectDropdownInput({
+  onSubmitValue,
+  canOpenList,
+  toggleList,
+  listOpen,
+}: IMultiSelectDropdownInputProps) {
+  const [inputValue, setInputValue] = useState<string>("");
+
+  const updateInput: ReactEventHandler<HTMLInputElement> = (event) => {
+    const value = (event.target as HTMLInputElement).value;
+    if (!value.length) return;
+    setInputValue(value);
+  };
+
+  const submitValue: KeyboardEventHandler<HTMLInputElement> = (event) => {
+    if (event.key !== "Enter" || !inputValue.length) return;
+    onSubmitValue(capitalizeFirstLetter(inputValue));
+    (event.target as HTMLInputElement).value = "";
+  };
+
+  const onInputFocus: ReactEventHandler<HTMLInputElement> = () => {
+    toggleList(true);
+  };
+
+  const onChevronClick: ReactEventHandler<HTMLButtonElement> = () => {
+    toggleList();
+  };
+
+  return (
+    <div className={styles.inputWrapper}>
+      <input
+        className={styles.input}
+        type="text"
+        onKeyDown={submitValue}
+        onChange={updateInput}
+        onInput={updateInput}
+        onFocus={onInputFocus}
+        placeholder="Add and select your fields of interests"
+        defaultValue={inputValue}
+      />
+      {canOpenList ? (
+        <button type="button" onClick={onChevronClick}>
+          <FaIcon
+            iconName="FaChevronDown"
+            className={clx(styles.icon, { [styles.open]: listOpen })}
+          />
+        </button>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 }
--- src/components/MultiSelectDropdown/types.ts
+++ src/components/MultiSelectDropdown/types.ts
@@ -1,26 +1,30 @@
-import type { ReactEventHandler } from "react"
+import type { ReactEventHandler } from "react";
 
 interface IMultiSelectDropdownOption {
-    value: string
-    id: string
-    selected: boolean
+  value: string;
+  id: string;
+  selected: boolean;
 }
 
-interface IMultiSelectDropdownOptionProps extends Omit<IMultiSelectDropdownOption, 'id'> {
-    onClick: ReactEventHandler
+interface IMultiSelectDropdownOptionProps
+  extends Omit<IMultiSelectDropdownOption, "id"> {
+  onClick: ReactEventHandler;
 }
 
 interface IMultiSelectDropdownOptionsProps {
-    options: IMultiSelectDropdownOption[]
+  options: IMultiSelectDropdownOption[];
 }
 
 interface IMultiSelectDropdownInputProps {
-    onKeyDown: ReactEventHandler
+  canOpenList: boolean;
+  listOpen: boolean;
+  onSubmitValue: (val: string) => void;
+  toggleList: (val?: boolean) => void;
 }
 
 export type {
-    IMultiSelectDropdownInputProps,
-    IMultiSelectDropdownOption,
-    IMultiSelectDropdownOptionProps,
-    IMultiSelectDropdownOptionsProps
-}
+  IMultiSelectDropdownInputProps,
+  IMultiSelectDropdownOption,
+  IMultiSelectDropdownOptionProps,
+  IMultiSelectDropdownOptionsProps,
+};
